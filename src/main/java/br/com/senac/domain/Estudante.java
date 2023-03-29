b17bfc66cf5c0b011f93d0786b9bf5283d61ca07
SENACRJEDU\36124872021.1N <yuri_paulino@hotmail.com>
2023-03-28 21:36:45 -0300

    Finalizando feature: estudante no banco de dados

--- src/main/java/br/com/senac/domain/Estudante.java
+++ src/main/java/br/com/senac/domain/Estudante.java
@@ -8,36 +8,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
 public class Estudante {
-	@Getter
-	@Setter
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Getter
-	@Setter
 	private String email;
-	
-	@Getter
-	@Setter
 	private LocalDate dataNascimento;
-	
 	private String nome;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 }
